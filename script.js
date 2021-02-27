8032508633015df45956a9aff249c739201842e5
Zack <ahmedsakaria32@gmail.com>
2021-02-26 22:52:19 -0600

    corrected the issue with the alphabetchar, on line 63

--- script.js
+++ script.js
@@ -60,11 +60,10 @@ var numberchar = [1,2,3,4,5,6,7,8,9,10];
  }
  function getRandomletter() {
   var alphabetChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  return alphbetChar[Math.floor(Math.random() * alphabetChar.length)];
-  
-
+  return alphabetChar[Math.floor(Math.random() * alphabetChar.length)];
    
  }
+ console.log(getRandomletter());
 console.log(getRandomSymbol());
 console.log(getRandomNumber());
 // Write password to the #password input
